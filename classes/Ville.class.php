200cbedcd4a2585fc36dc527d743034013f6b79b
Mardaspan <imbert.kevin94@gmail.com>
2014-10-14 18:37:43 +0200

    ajout ville et villeManager

--- classes/Ville.class.php
+++ classes/Ville.class.php
@@ -1,4 +1,39 @@
 <?php
 class Ville{
+	private $vil_num;
+	private $vil_nom;
 	
+	public function __construct($valeurs = array()) {
+		if(!empty($valeurs)) {
+			$this->affecte($valeurs);
+		}
+	}
+	
+	public function affecte ($donnees) {
+		foreach ($donnees as $attribut => $valeur) {
+			switch ($attribut) {
+				case 'vil_num': $this->setVilNum($valeur); break;
+					
+				case 'vil_nom': $this->setVilNom($valeur); break;
+									
+				default:
+					echo "Erreur case : fonction affecte - classe Ville ! \n";
+					break;
+			}
+		}
+	}
+	
+	public function getVilNum() {
+		return $this->vil_num;
+	}
+	public function setVilNum ($id) {
+		$this->vil_num = $id;
+	}
+	public function getVilNom() {
+		return $this->vil_nom;
+	}
+	public function setVilNom($nom) {
+		$this->vil_nom = $nom;
+	}
 }
+?>
--- classes/VilleManager.class.php
+++ classes/VilleManager.class.php
@@ -1,4 +1,31 @@
 <?php
 class VilleManager{
+	private $db;
 	
+	public function __construct($db) {
+		$this->db = $db;
+	}
+	
+	public function add ($ville) {
+		$requete = $this->db->prepare(
+		'INSERT INTO ville (vil_nom) VALUES (:nom);');
+		$requete->binValue(':nom', $ville->getVilNom());
+		
+		$retour=$requete->execute();
+		return $retour;
+	}
+	
+	public function getAllVilles() {
+		$listeVilles = array();
+		$sql='SELECT vil_num, vil_nom FROM ville ORDER BY vil_nom';
+		$requete=$this->db->prepare($sql);
+		$requete->execute();
+		while ($ville = $requete->fetch(PDO::FETCH_ASSOC)) {
+			$listeVilles[] = new Ville($ville);
+		}
+		$requete->closeCursor();
+		
+		return $listeVilles;
+	}
 }
+?>
