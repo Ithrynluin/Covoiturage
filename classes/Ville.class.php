c0e30bb39f87776cdc80faf8be2fb084ecbb29d9
clem19s <clemj1900@gamil.com>
2014-10-14 18:39:21 +0200

    ajout ville et ville manager

--- classes/Ville.class.php
+++ classes/Ville.class.php
@@ -1,4 +1,44 @@
 <?php
 class Ville{
-	
+	private $num;
+    private $nom;
+    
+    public function __construct($valeurs = array()){
+        if(!empty($valeurs)){
+            affecte($valeurs);
+        }
+    }
+    
+    public function affecte($donnees){
+        foreach ($donnees as $attribut => $value) {
+            switch ($attribut) {
+                case 'vil_num':
+                    $this->setNum($value);
+                    break;
+                case 'vil_nom':
+                    $this->setNom($value);
+                default:
+                    break;
+            }
+        }
+    }
+    
+    public function getNum(){
+        $this->num;
+    }
+    
+    public function setNum($num){
+        $this->num = $num;
+    }
+    
+    public function getNom(){
+        return $this->nom;
+    }
+    
+    public function setNom($nom){
+        $this->nom = $nom;        
+    }
+    
 }
+
+?>
--- classes/VilleManager.class.php
+++ classes/VilleManager.class.php
@@ -1,4 +1,18 @@
 <?php
 class VilleManager{
-	
+	private $db;
+    
+    public function __construct($db){
+        $this->db = $db;
+    }
+    
+    public function add($ville){
+        $sql='INSERT INTO ville (vil_num) values :nom';
+        $requete = $this->db->prepare($sql);
+        $requete->bindValue(':nom', $ville->getNom());
+        $retour = $requete->execute();
+        return $retour;
+    }
 }
+
+?>
