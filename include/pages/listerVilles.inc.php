065867fde76d6c477e65404490d5cb3d07291cf7
Mardaspan <imbert.kevin94@gmail.com>
2014-10-14 19:14:27 +0200

    ajout liste de villes

--- include/pages/listerVilles.inc.php
+++ include/pages/listerVilles.inc.php
@@ -1,2 +1,32 @@
+<h1>Liste des villes</h1>
 <?php
+$pdo=new Mypdo();
+$listeVilles=new VilleManager(pdo);
 
+$nb=count($listeVilles);
+?>
+<p>Actuellement	villes <?php echo $nb ?>sont enregistrées</p>
+<table>
+	<tr>
+		<th>Numéro</th>
+		<th>Nom</th>
+	</tr>
+	<?php
+	foreach ($listeVilles as $value) {
+		?>
+		<tr>
+		<td>
+			<?php
+			echo $value.getVilNum();
+			?>
+		</td>
+		<td>
+			<?php
+			echo $value.getVilNom();
+			?>
+		</td>
+		</tr>
+		<?php
+	}
+	?>
+</table>
