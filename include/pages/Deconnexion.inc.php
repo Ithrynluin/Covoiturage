6b433c583a7d61e22ece6c5c6f61c2f49feba60c
clem19s <clemj1900@gmail.com>
2014-11-18 16:13:45 +0100

    Deconnexion redirige forcément vers l'accueil

--- include/pages/Deconnexion.inc.php
+++ include/pages/Deconnexion.inc.php
@@ -1,7 +1,6 @@
 <?php
     require_once("include/functions.inc.php");
-    setPagePrecedente("http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ); 
 	$_SESSION['utilisateur'] = "";
     $_SESSION['per_num'] = "";
-    header("Location: ".getPagePrecedente("http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ));
+    header("Location: "."index.php" );
 ?>
