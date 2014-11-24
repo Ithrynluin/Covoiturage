e407d3e137d1517423820852bcec46f8d1fb4dba
clem19s <clemj1900@gmail.com>
2014-11-24 10:08:02 +0100

    Ajout de la page chercher trajet, il manque l'affichage du tableau

--- classes/ProposeManager.class.php
+++ classes/ProposeManager.class.php
@@ -19,4 +19,50 @@ class ProposeManager{
         $retour = $requete->execute();
         return $retour;
     }
+    
+    
+    public function getVilleDepartTrajet(){
+        $listeVille = array();
+        $sql = 'select distinct vil_num, vil_nom 
+                from ville 
+                where vil_num IN (select vil_num1 
+                    from parcours p inner join propose pr on pr.par_num = p.par_num 
+                    where pro_sens = 0) 
+                OR vil_num IN (select vil_num2 
+                    from parcours p inner join propose pr on pr.par_num = p.par_num 
+                    where pro_sens = 1)';
+        $requete = $this->db->prepare($sql);
+        $requete->execute();
+        
+        while($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
+            $listeVille[] = new Ville($ligne);
+        }
+        
+        $requete->closeCursor();
+        return $listeVille;
+    }
+    
+    public function getTrajetWithParam($par_num, $date_debut, $date_fin, $heure, $sens){
+        $listeTrajet = array();    
+        $sql='SELECT * from propose 
+                Where par_num = :par_num 
+                AND pro_date BETWEEN :date_debut AND :date_fin 
+                AND pro_time >= :pro_time 
+                AND pro_sens = :pro_sens';
+        
+       $requete = $this->db->prepare($sql);
+       $requete->bindValue(":par_num", $par_num);
+       $requete->bindValue(":date_debut", $date_debut);
+       $requete->bindValue(":date_fin", $date_fin);
+       $requete->bindValue(":pro_time", $heure);
+       $requete->bindValue(":pro_sens", $sens);
+       $requete->execute();
+       
+       while($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
+           $listeTrajet[] = new Propose($ligne);
+       }
+       
+       $requete->closeCursor();
+       return $listeTrajet;
+    }
 }
--- include/pages/ChercherTrajet.inc.php
+++ include/pages/ChercherTrajet.inc.php
@@ -1,1 +1,79 @@
 <h1>Rechercher un trajet</h1>
+<?php 
+$pdo = new Mypdo();
+if(empty($_POST['vildepart']) && empty($_POST['vilarr']) && empty($_POST['depart']) && empty($_POST['precision']) && empty($_POST['heure'])){ ?>
+    <form action="index.php?page=10" method="post">
+        <p>
+            <label for="vildepart"> Ville de départ : </label>
+            <select id="vildepart" name="vildepart" class="champ">
+<?php           
+                $proposeManager = new ProposeManager($pdo);
+                $listeVille = $proposeManager->getVilleDepartTrajet(); 
+                foreach ($listeVille as $key => $value) { ?>
+                    <option value=<?php echo '"'.$value->getVilNum().'"'; ?>><?php echo $value->getVilNom(); ?></option>  
+<?php           } ?>
+            </select>
+            <input type="submit" value="Valider" class="bouton"/>
+        </p>
+    </form>
+<?php
+}else if(!empty($_POST['vildepart']) && empty($_POST['vilarr']) && empty($_POST['depart']) && empty($_POST['precision']) && empty($_POST['heure'])){ 
+   $villeManager = new VilleManager($pdo);
+   $villeDepart = $villeManager->getVille($_POST['vildepart']);
+   $listeVilleArrive = $villeManager->getVilleParcoursArrive($_POST['vildepart']);
+    $_SESSION['vildepart'] = $_POST['vildepart']; ?> 
+   <form action="index.php?page=10" method="post">
+       <table>
+           <tr>
+               <td>Ville de départ : </td>
+               <td><?php echo $villeDepart->getVilNom(); ?></td>
+               
+               <td><label>Ville d'arrivée : <label>+</td>
+               <td>
+                   <select name="vilarr" class="champ">
+<?php                   foreach ($listeVilleArrive as $value) { ?>
+                        <option value=<?php echo $value->getVilNum() ?>><?php echo $value->getVilNom() ?></option>
+<?php                   } ?>
+                   </select>
+                </td>
+           </tr>
+           <tr>
+               <td><label>Date de départ :</label></td>
+               <td> <input type="date" id="depart" name="depart" class="champ" value=<?php echo '"'.date("d/m/Y").'"'; ?>/></td>
+               
+               <td><label>Précision :</label></td>
+               <td>
+                   <select name="precision" class="champ">
+                       <option value="0">Ce jour</option>
+                       <option value="1">+/- 1 jour</option>
+                       <option value="2">+/- 2 jour</option>
+                       <option value="3">+/- 3 jour</option>
+                   </select>
+               </td>
+           </tr>
+           <tr>
+               <td><label>A partir de :</label></td>
+               <td>
+                   <select name="heure" class="champ">
+<?php                   for($i=0; $i < 24; $i++){ ?>
+                            <option value=<?php echo '"'.$i.'"'; ?>><?php echo $i."h"; ?></option>
+<?php                   }?>            
+                   </select>
+               </td>
+           </tr>
+       </table>
+       <p>
+           <input type="submit" value="Valider" class="bouton"/>
+       </p>
+   </form>
+<?php    
+}else if(empty($_POST['vildepart']) && !empty($_POST['vilarr']) && !empty($_POST['depart']) && isset($_POST['precision']) && isset($_POST['heure'])){
+    $parcoursManager = new ParcoursManager($pdo);
+    $parEtSens = $parcoursManager->getParcoursEtSensAvecVilles($_SESSION['vildepart'], $_POST['vilarr']);
+    $dateDebut = addJours($_POST['depart'], -$_POST['precision']);
+    $dateFin = addJours($_POST['depart'], $_POST['precision']);
+    
+    $proposeManager = new ProposeManager($pdo);
+    $liste = $proposeManager->getTrajetWithParam($parEtSens['parcours'], $dateDebut, $dateFin, $_POST['heure'], $parEtSens['sens']);
+    echo "<pre>";var_dump($liste);echo "</pre>";
+} ?>
